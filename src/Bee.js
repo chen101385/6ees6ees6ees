1d487f89c5d8224de5642202779359354b1037f0
chen101385 <chen1385@gmail.com>
2017-12-04 15:31:30 -0800

    finished es6 classes exercise

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,10 @@
-class Bee {
-  // TODO..
+class Bee extends Grub {
+  constructor(age, color, job) {
+  //when should we pass inputs to constructor function?
+    //under what circumstances should we pass inputs to super function?
+    super(age, color, job);
+    this.age = 5;
+    this.color = 'yellow';
+    this.job = 'Keep on growing';
+  }
 };
--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,12 @@
-class ForagerBee {
-  // TODO..
+class ForagerBee extends Bee {
+  constructor (age, job, canFly, treasureChest) {
+    super(age, job);
+    this.age = 10;
+    this.job = 'find pollen';
+    this.canFly = true;
+    this.treasureChest = [];
+  }
+  forage() {
+    this.treasureChest.push('treasure');
+  };
 };
